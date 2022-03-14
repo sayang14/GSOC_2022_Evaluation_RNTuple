df823e0eaef79607bf0db2f6e61014ccbde69ac1
Sayandeep Ghosh <77008026+sayang14@users.noreply.github.com>
2022-03-14 23:09:37 +0530

    Create gsoc_eval_RNTuple.C

---
+++ gsoc_eval_RNTuple.C
@@ -0,0 +1,125 @@
+R__LOAD_LIBRARY(ROOTNTuple)
+#include <ROOT/RNTuple.hxx>
+#include <ROOT/RNTupleModel.hxx>
+ 
+#include <TCanvas.h>
+#include <TH1I.h>
+#include <TROOT.h>
+#include <TString.h>
+ 
+#include <cassert>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <utility>
+
+using RNTupleModel = ROOT::Experimental::RNTupleModel;
+using RNTupleReader = ROOT::Experimental::RNTupleReader;
+using RNTupleWriter = ROOT::Experimental::RNTupleWriter;
+
+constexpr char const* kNTupleFileName = "gsoc_eval_RNTuple.root";
+
+std::vector<string> h;
+std::vector<std::shared_ptr<int>> v1;
+std::vector<std::shared_ptr<float>> v2;
+std::pair<std::shared_ptr<int>,std::shared_ptr<float>> p;
+
+void Ingest() {
+    // The input file temperature_prague.dat is a copy of the temperature data base in Prague from 1775 to 2004
+    //"/tree/temperature_Prague.csv"
+    char filename[100];
+    //I specifically mentioned the tuitorials directory since I found most of the datasets are stored there
+    std::cout << "Enter the csv filename path(no more than 100 characters) present in the installation/tutorials directory of ROOT):";
+    std::cin >> filename;
+    ifstream fin(gROOT->GetTutorialDir() + "/tree/" + filename);
+    assert(fin.is_open());
+    //creating unique pointer to an empty data model
+    auto model = RNTupleModel::Create();
+    
+    std::string record_header,word;
+    getline(fin,record_header);
+    std::istringstream iss(record_header);
+    while(getline(iss,word,','))
+    {
+      h.push_back(word);
+    }
+    //std::cout<<h.size()<<std::endl;
+      for(int i=0;i<h.size();i++)
+      {
+        if(h[i].find("int")!= string::npos){
+        auto fld = model->MakeField<int>(h[i]);
+        v1.push_back(fld);
+        //std::cout<<"int"<<std::endl;
+        }
+        else if(h[i].find("float")!= string::npos) {
+        auto fld = model->MakeField<float>(h[i]);
+        v2.push_back(fld);
+        }
+      }
+    
+    // We hand-over the data model to a newly created ntuple of name "new_ntuple", stored in kNTupleFileName
+    auto ntuple = RNTupleWriter::Recreate(std::move(model), "new_ntuple", kNTupleFileName);
+    
+    auto size = h.size();
+    std::string record;
+    while(std::getline(fin,record)) {
+       std::istringstream iss(record);
+       //iss>>*fld1>>*fld2>>*fld3>>*fld4;
+       for(auto fld : v1)iss>>*fld;
+       for(auto fld : v2)iss>>*fld;
+       ntuple->Fill();
+    }
+    
+       
+}
+
+void Analyze() {
+   // Get a unique pointer to empty RNTuple models
+   auto model = RNTupleModel::Create();
+   
+   std::string entry;
+   std::cout<<"Enter the entry whose distribution you wish to see in the following way(Name:Type)!"<<std::endl;
+   std::cin>>entry;
+   
+   //defining field(fldTemp from Ingest function where it was declared) that is needed for reading
+   if(entry.find("int")!=string::npos){auto fld = model->MakeField<int>(entry);
+     p.first = fld;
+   }
+   else if(entry.find("float")!=string::npos){auto fld = model->MakeField<float>(entry);
+     p.second = fld;
+   }	
+   
+   // Quick overview of the ntuple and list of fields.
+   auto ntuple = RNTupleReader::Open(std::move(model),"new_ntuple", kNTupleFileName);
+   ntuple->PrintInfo();
+   
+   std::cout << "The first entry in JSON format:" << std::endl;
+   ntuple->Show(0);
+   
+   auto c = new TCanvas("c", "", 200, 10, 700, 500);
+   TH1I h("h", " Distribution for your entry", 100, -100, 100);
+   h.SetFillColor(40);
+   
+   
+   if(entry.find("int")!=string::npos) {
+       for (auto entryId : *ntuple) {
+       ntuple->LoadEntry(entryId);
+       h.Fill(*p.first);
+      }
+   } else if(entry.find("float")!=string::npos) {
+      for (auto entryId : *ntuple) {
+      ntuple->LoadEntry(entryId);
+      h.Fill(*p.second); 
+      }
+   }
+   
+   h.DrawCopy();
+}
+
+void gsoc_eval_RNTuple() {
+   Ingest();
+   Analyze();
+}
